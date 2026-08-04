eaf22b3ff49b48cfab4acbbb88f4300890b935a1
Parv Agrawal <196064064+parvagr247@users.noreply.github.com>
2026-08-04 19:07:20 +0530

    feat(backend) : Initialization

---
+++ backend/src/main/java/com/cbp7/CbpBackendApplication.java
@@ -0,0 +1,13 @@
+package com.cbp7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CbpBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CbpBackendApplication.class, args);
+	}
+
+}
