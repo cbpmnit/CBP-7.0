4bd867746eab4a812ae57f2d7ef1753a8decbc67
Parv Agrawal <196064064+parvagr247@users.noreply.github.com>
2026-08-15 14:29:54 +0530

    timezone resolve

--- backend/src/main/java/com/cbp7/CbpBackendApplication.java
+++ backend/src/main/java/com/cbp7/CbpBackendApplication.java
@@ -7,11 +7,19 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.EnableAsync;
 
+import jakarta.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication
 @EnableJpaAuditing
 @EnableAsync
 @EnableScheduling
 public class CbpBackendApplication {
+
+	@PostConstruct
+	public void init() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Kolkata"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(CbpBackendApplication.class, args);
